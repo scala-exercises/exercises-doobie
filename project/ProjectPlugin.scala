bbc7ffee5b0c39a2659b60169d6fbf7f981197cc
Scala Steward <me@scala-steward.org>
2020-02-21 05:09:07 +0100

    Update scalatest to 3.1.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -19,7 +19,7 @@ object ProjectPlugin extends AutoPlugin {
       val doobie: String              = "0.8.6"
       val cats: String                = "2.0.0"
       val shapeless: String           = "2.3.3"
-      val scalatest: String           = "3.1.0"
+      val scalatest: String           = "3.1.1"
       val scalatestplusScheck: String = "3.1.0.0-RC2"
       val scalacheck: String          = "1.14.3"
       val scalacheckShapeless: String = "1.2.4"
