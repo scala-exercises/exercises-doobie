f4734f20f31c0e5fcb1a94e62828043df52c7cfe
Juan Pedro Moreno <4879373+juanpedromoreno@users.noreply.github.com>
2020-04-27 22:00:24 +0200

    Update ProjectPlugin.scala

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -15,7 +15,7 @@ object ProjectPlugin extends AutoPlugin {
       val doobie: String              = "0.9.0"
       val shapeless: String           = "2.3.3"
       val scala: String               = "2.13.2"
-      val scalaExercises: String      = "0.6.0-SNAPSHOT"
+      val scalaExercises: String      = "0.6.0"
       val scalacheck: String          = "1.14.3"
       val scalacheckShapeless: String = "1.2.5"
       val scalatest: String           = "3.1.1"
